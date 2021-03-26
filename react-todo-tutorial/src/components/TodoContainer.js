3ec21377258a2e618a855df850948747cae15f4e
Felipe Pimenta <felimenta97@gmail.com>
2021-03-26 15:29:43 -0300

    Added checkbox to todo item

--- react-todo-tutorial/src/components/TodoContainer.js
+++ react-todo-tutorial/src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import TodoList from "./TodoList"
+import Header from "./Header"
 
 class TodoContainer extends React.Component {
 
@@ -27,6 +28,7 @@ class TodoContainer extends React.Component {
     render() {
         return (
             <>
+                <Header />
                 <TodoList todos={this.state.todos} />
             </>
         )
