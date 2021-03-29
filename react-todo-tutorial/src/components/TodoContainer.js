02bb08b13aa7a387c6345226aa3de073eef9f198
Felipe Pimenta <felimenta97@gmail.com>
2021-03-29 10:18:16 -0300

    Lidando com o inputtodo

--- react-todo-tutorial/src/components/TodoContainer.js
+++ react-todo-tutorial/src/components/TodoContainer.js
@@ -4,22 +4,24 @@ import TodoList from "./TodoList"
 import Header from "./Header"
 import InputTodo from "./InputTodo"
 
+import { v4 as uuidv4 } from "uuid";
+
 class TodoContainer extends React.Component {
 
     state = {
         todos: [
             {
-                id: 1,
+                id: uuidv4(),
                 title: "Setup development enviroment",
                 completed: true
             },
             {
-                id: 2,
+                id: uuidv4(),
                 title: "Develop website and add content",
                 completed: false
             },
             {
-                id: 3,
+                id: uuidv4(),
                 title: "Deploy to live server",
                 completed: false
             }
@@ -52,10 +54,23 @@ class TodoContainer extends React.Component {
         })
     }
 
+    addTodoItem = title => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false
+        };
+        
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
+    }
+
     render() {
         return (
             <>
                 <Header />
+                <InputTodo addTodoProps={this.addTodoItem}/>
                 <TodoList
                     todos={this.state.todos}
                     handleChangeProps={this.handleChange}
